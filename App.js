29c7c3aa7b37a4acca58efeccd15ebdb6479f276
Robin Sola <rsolarms@gmail.com>
2018-07-13 12:58:27 -0700

    create PoemSearch component in App component

--- App.js
+++ App.js
@@ -10,17 +10,34 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, NavigatorIOS} from 'react-native';
 
 type Props = {};
-export default class App extends Component<Props> {
+
+class PoemSearch extends Component<{}> {
   render() {
-    return <Text style={styles.container}>Find Poems</Text>;
+    return <Text style={styles.containerSearch}>Find Poems</Text>;
+  }
+}
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Stanza',
+          component: PoemSearch,
+        }}/>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerSearch: {
     fontSize: 18,
     textAlign: 'center',
     color: '#656565',
     marginTop: 65,
+  },
+  container: {
+    flex: 1,
   },
 });
