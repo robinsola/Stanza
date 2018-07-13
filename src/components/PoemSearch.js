a05debbb6f328be5cdea9cc0ceeb76af87f27bd6
Robin Sola <rsolarms@gmail.com>
2018-07-13 13:39:59 -0700

    add styles

--- src/components/PoemSearch.js
+++ src/components/PoemSearch.js
@@ -5,24 +5,24 @@ import { StyleSheet, Text, TextInput, View, Button, ActivityIndicator, Image, } 
 export default class PoemSearch extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.body}>
         <Text style={styles.text}>Pick the Occasion:</Text>
-        <Text style={styles.text}>Search Form Here</Text>
+        <Text style={styles.text}>(Search Form Here)</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center',
+  },
+  text: {
     marginBottom: 20,
     fontSize: 18,
     textAlign: 'center',
     color: '#656565',
-  },
-  text: {
-    padding: 30,
-    marginTop: 65,
-    alignItems: 'center',
   },
 });
