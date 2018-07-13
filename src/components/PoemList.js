fe87af1e65dcb92eba9127b8160e7f841fbf55b0
Robin Sola <rsolarms@gmail.com>
2018-07-13 16:35:06 -0700

    add styles for items

--- src/components/PoemList.js
+++ src/components/PoemList.js
@@ -2,25 +2,84 @@
 import React, { Component } from 'react';
 import { StyleSheet, Image, View, TouchableHighlight, FlatList, Text } from 'react-native';
 
-export default class SearchResults extends Component<{}> {
-  _keyExtractor = (item, index) => index;
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
 
-  _renderItem = ({item}) => {
+  render() {
+    const item = this.props.item;
+    const price = item.price_formatted.split(' ')[0];
     return (
-      <TouchableHighlight underlayColor='#dddddd'>
+      <TouchableHighlight
+        onPress={this._onPress}
+        underlayColor='#dddddd'>
         <View>
-          <Text>{item.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: item.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                numberOfLines={1}>{item.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
-  };
+  }
+}
+
+export default class SearchResults extends Component<{}> {
+  _keyExtractor = (item, index) => index;
+
+  _renderItem = ({item, index}) => (
+  <ListItem
+    item={item}
+    index={index}
+    onPressItem={this._onPressItem}
+  />
+);
+
+_onPressItem = (index) => {
+  console.log("Pressed row: "+index);
+};
 
   render() {
     return (
       <FlatList
         data={this.props.listings}
         keyExtractor={this._keyExtractor}
-        renderItem={this._renderItem}/>
+        renderItem={this._renderItem}
+      />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+});
